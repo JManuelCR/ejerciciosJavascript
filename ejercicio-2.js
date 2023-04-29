0d6be5f088170163bc1e1204f22e27128cdf0bdc
Jose Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2023-04-29 18:45:29 -0500

    ejercicio 2

---
+++ ejercicio-2.js
@@ -0,0 +1,60 @@
+let mentors = [
+    ["Naomi", "López"],
+    ["Israel", "Salinas"],
+    ["Charles", "Silva"],
+    ["Rose", "Ortega"],
+  ];
+  let koders = [
+    {
+      name: "Verónica Cruz",
+      scores: {
+        html: 10,
+        css: 9.2,
+        js: 9.7,
+        bootstrap: 8,
+      },
+      isGraduated: true,
+    },
+    {
+      name: "Miguel Aceves",
+      scores: {
+        html: 8.7,
+        css: 10,
+        js: 8.3,
+        bootstrap: 9.8,
+      },
+      isGraduated: false,
+    },
+    {
+      name: "Antonio Cano",
+      scores: {
+        html: 9.2,
+        css: 10,
+        js: 10,
+        bootstrap: 6.5,
+      },
+      isGraduated: false,
+    },
+  ];
+
+/**
+ 2.- Necesito una lista con los nombres y promedios generales de todos los koders
+ */
+    
+ /*
+  Con el array koders que contiene objetos, devolver las propiedades name y calcular el promedio de las materia dentro de propiedad scores
+*/
+
+const getNameAndAverage = (kodersList) =>  {
+    let kodersAndAverage = [];
+    for(let i = 0; i < kodersList.length; i++){
+        let name = kodersList[i].name; 
+        let scoresLength = Object.keys(kodersList[i].scores).length  
+        let koderAverage = (kodersList[i].scores.html + kodersList[i].scores.css + kodersList[i].scores.js + kodersList[i].scores.bootstrap) / scoresLength
+        kodersAndAverage.push(`El koder ${name} tiene un promedio de ${koderAverage}`) 
+    }
+    return kodersAndAverage
+}
+
+let prueba1 = getNameAndAverage(koders);
+console.log(prueba1);
