43998386cf1381bf5d418939aaa325349d751765
Jose Manuel Cabrera Rojas <josemanuelcabrera2011@gmail.com>
2023-04-29 16:13:40 -0500

    ejercicio numero 1 construido

---
+++ EJercicio-1.js
@@ -0,0 +1,45 @@
+let mentors = [
+    ["Naomi", "López"],
+    ["Israel", "Salinas"],
+    ["Charles", "Silva"],
+    ["Rose", "Ortega"],
+  ];
+  let koders = [
+    {
+      name: "Verónica Cruz",
+      scores: {
+        html: 10,
+        css: 9.2,
+        js: 9.7,
+        bootstrap: 8,
+      },
+      isGraduated: true,
+    },
+    {
+      name: "Miguel Aceves",
+      scores: {
+        html: 8.7,
+        css: 10,
+        js: 8.3,
+        bootstrap: 9.8,
+      },
+      isGraduated: false,
+    },
+    {
+      name: "Antonio Cano",
+      scores: {
+        html: 9.2,
+        css: 10,
+        js: 10,
+        bootstrap: 6.5,
+      },
+      isGraduated: false,
+    },
+  ];
+
+/*
+      1.- Necesito una lista que contenga los nombres completos de los mentores ordenados alfabéticamente
+*/
+
+
+
